09c5040f2dc972e7d6bf11857520f0286d8e9aca
quartersmacbooki <shubhammourya80@gmail.com>
2019-09-09 13:57:03 +0530

    Adding new library

--- app/src/main/java/com/quarter/pingmelive/sampleActivity.java
+++ app/src/main/java/com/quarter/pingmelive/sampleActivity.java
@@ -3,6 +3,7 @@ package com.quarter.pingmelive;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class sampleActivity extends AppCompatActivity {
 
@@ -10,5 +11,6 @@ public class sampleActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sample);
+        Toast.makeText(getApplicationContext(),"Blank Activity",Toast.LENGTH_LONG).show();
     }
 }
