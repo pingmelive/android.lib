fab186d46eef8bb32b086f1592400178ef95c976
quartersmacbooki <shubhammourya80@gmail.com>
2019-09-16 10:44:41 +0530

    Added sample app

--- app/src/main/java/com/pingmelive/sampleActivity.java
+++ app/src/main/java/com/pingmelive/sampleActivity.java
@@ -3,6 +3,8 @@ package com.pingmelive;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class sampleActivity extends AppCompatActivity {
@@ -11,6 +13,29 @@ public class sampleActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sample);
-        Toast.makeText(getApplicationContext(),"Blank Activity!",Toast.LENGTH_LONG).show();
+
+        String userID = "userabc";
+
+        Button forceStop = findViewById(R.id.forceStop);
+        forceStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                throw new RuntimeException("Here is an error so that you can test!");
+
+            }
+        });
+
+        Button simpleEvent = findViewById(R.id.simpleEvent);
+        simpleEvent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Toast.makeText(sampleActivity.this, "Simple event sent", Toast.LENGTH_SHORT).show();
+                pingMeLive.simpleEvent("Simple event","This is a simple event!!");
+
+
+            }
+        });
     }
 }
